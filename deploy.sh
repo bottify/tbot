5d1bdf373aafe5a4a24fef4a54588793d318204c
Tamce <tamce@outlook.com>
2022-05-06 01:49:26 +0800

    misc: add deploy script to vcs

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git pull origin master --force
+git submodule update
+source /opt/rh/devtoolset-7/enable
+mkdir bin 2>/dev/null
+go build -o bin/ .
+killall tbot
+nohup bin/tbot >>bin/tbot.log 2>&1 &
